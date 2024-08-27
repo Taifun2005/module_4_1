41bc5ebc606b9274b512f311f2d43064e8adcfa1
Taifun2005 <lider2005@yandex.ru>
2024-08-28 08:50:52 +1100

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,10 @@
+def divide(first, second):
+    # result = 0
+    if second == 0:
+        return 'Ошибка11'
+    else:
+        return first / second
+
+
+result = divide(10, 0)
+print(result)
---
+++ module_4_1.py
@@ -0,0 +1,14 @@
+from fake_math import divide as fm
+from true_math import divide as tm
+
+print(fm(10, 2))
+print(tm(10, 3))
+
+result1 = fm(69, 3)
+result2 = fm(3, 0)
+result3 = tm(49, 7)
+result4 = tm(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
